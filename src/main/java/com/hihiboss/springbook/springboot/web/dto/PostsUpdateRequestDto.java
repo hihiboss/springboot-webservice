52112e8e7e11138c4e33cc67ce79fd333d97d2e0
hihiboss <junhui820@gmail.com>
2019-12-12 18:35:41 +0900

    impl: Implement DTO, service and controller for updating/finding the post

---
+++ src/main/java/com/hihiboss/springbook/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,11 @@
+package com.hihiboss.springbook.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+}
