6412ed692994f2cc809c6de484208758232cebec
hihiboss <junhui820@gmail.com>
2019-12-17 22:24:56 +0900

    impl: Implement BaseTimeEntity class and test case

--- src/test/java/com/hihiboss/springbook/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/hihiboss/springbook/springboot/domain/posts/PostsRepositoryTest.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,5 +49,26 @@ public class PostsRepositoryTest {
 
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void registerBaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 12, 17, 22, 20, 00);
+        postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts = postsList.get(0);
+
+        // then
+        System.out.println(">>>>>>>> createDate=" + posts.getCreatedDate()+", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
