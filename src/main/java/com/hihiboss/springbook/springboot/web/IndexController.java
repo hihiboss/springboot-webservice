c83243a9a1df05cd8e8d39c41802cb42f3c8c6e7
hihiboss <junhui820@gmail.com>
2020-01-28 02:25:19 +0900

    refactor: Refactor IndexController with @LoginUser annotation

--- src/main/java/com/hihiboss/springbook/springboot/web/IndexController.java
+++ src/main/java/com/hihiboss/springbook/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hihiboss.springbook.springboot.web;
 
+import com.hihiboss.springbook.springboot.config.auth.LoginUser;
 import com.hihiboss.springbook.springboot.config.auth.dto.SessionUser;
 import com.hihiboss.springbook.springboot.domain.user.User;
 import com.hihiboss.springbook.springboot.service.PostsService;
@@ -21,9 +22,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
