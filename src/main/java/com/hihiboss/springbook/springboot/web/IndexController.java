1103a3b837988086df55d97de301543b817fda88
hihiboss <junhui820@gmail.com>
2020-01-28 01:11:07 +0900

    impl: Implement Google login front-end and session service

--- src/main/java/com/hihiboss/springbook/springboot/web/IndexController.java
+++ src/main/java/com/hihiboss/springbook/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.hihiboss.springbook.springboot.web;
 
+import com.hihiboss.springbook.springboot.config.auth.dto.SessionUser;
+import com.hihiboss.springbook.springboot.domain.user.User;
 import com.hihiboss.springbook.springboot.service.PostsService;
 import com.hihiboss.springbook.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
